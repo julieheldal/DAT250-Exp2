73df7d5828a54dc366b2911a0c67417bab7a3bee
julieheldal <578026@stud.hvl.no>
2021-09-20 16:45:33 +0200

    final changes

--- eclipselink/jpa-basic/src/main/java/no/hvl/dat250/jpa/basicexample/Main.java
+++ eclipselink/jpa-basic/src/main/java/no/hvl/dat250/jpa/basicexample/Main.java
@@ -14,22 +14,62 @@ public class Main {
     public static void main(String[] args) {
         factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
         EntityManager em = factory.createEntityManager();
-        // read the existing entries and write to console
-        Query q = em.createQuery("select t from Todo t");
-        List<Todo> todoList = q.getResultList();
-        for (Todo todo : todoList) {
-            System.out.println(todo);
-        }
-        System.out.println("Size: " + todoList.size());
 
-        // create new todo
+
         em.getTransaction().begin();
-        Todo todo = new Todo();
-        todo.setSummary("This is a test");
-        todo.setDescription("This is a test");
-        em.persist(todo);
-        em.getTransaction().commit();
 
+        Person person = createPerson("Max Mustermann");
+        Address address = createAddress("Inndalsveien",28);
+        CreditCard creditCard = createCreditCard(123245,-5000,-1000);
+        PinCode pinCode = createPinCode("123",1);
+        Bank bank = createBank("Pengebank");
+
+        creditCard.setPinCode(pinCode);
+        person.addAddress(address);
+        person.addCreditCard(creditCard);
+
+
+        em.persist(person);
+        em.persist(creditCard);
+        em.persist(pinCode);
+        em.persist(address);
+        em.persist(bank);
+
+        em.getTransaction().commit();
         em.close();
     }
+
+    public static Person createPerson(String name) {
+        Person person = new Person();
+        person.setName(name);
+        return person;
+    }
+
+    public static Address createAddress(String street, int number) {
+        Address address = new Address();
+        address.setStreet(street);
+        address.setNumber(number);
+        return address;
+    }
+    public static CreditCard createCreditCard(int number, int limit, int balance) {
+        CreditCard creditCard = new CreditCard();
+        creditCard.setNumber(number);
+        creditCard.setLimit(limit);
+        creditCard.setBalance(balance);
+        return creditCard;
+    }
+
+    public static PinCode createPinCode(String code, int count) {
+        PinCode pinCode = new PinCode();
+        pinCode.setPinCode(code);
+        pinCode.setCount(count);
+        return pinCode;
+    }
+
+    public static Bank createBank(String name) {
+        Bank bank = new Bank();
+        bank.setName(name);
+        return bank;
+    }
+
 }
